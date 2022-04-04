8dd3c6b733a330d73369e46c53f70a1d1ccbbb9f
ThoughtContagion <clittrell@soteria.io>
2022-04-04 10:20:00 -0400

    New funtionality

---
+++ Inspectors/Inspect-K8httpApplicationRouting.ps1
@@ -0,0 +1,45 @@
+function Inspect-K8httpApplicationRouting {
+    Try {
+        $results = @()
+        
+        $clusters = Get-AzAksCluster -WarningAction SilentlyContinue
+
+        Foreach ($cluster in $clusters){
+            If ($cluster.addonprofiles.keys -contains "httpApplicationRouting"){
+                $result = New-Object psobject
+                $result | Add-Member -MemberType NoteProperty -name 'Cluster' -Value $cluster.Name -ErrorAction SilentlyContinue
+                $result | Add-Member -MemberType NoteProperty -name 'Location' -Value $Cluster.Location -ErrorAction SilentlyContinue
+
+                $results += $result
+            }
+        }
+
+            
+        If ($results.Count -NE 0) {
+            $findings = @()
+            foreach ($x in $results){
+                $findings += "Cluster Name: $($x.Cluster), Location: $($x.Location)"
+            }
+            return $findings
+        }
+        
+        return $null
+    }
+    Catch {
+        Write-Warning "Error message: $_"
+    
+        $message = $_.ToString()
+        $exception = $_.Exception
+        $strace = $_.ScriptStackTrace
+        $failingline = $_.InvocationInfo.Line
+        $positionmsg = $_.InvocationInfo.PositionMessage
+        $pscommandpath = $_.InvocationInfo.PSCommandPath
+        $failinglinenumber = $_.InvocationInfo.ScriptLineNumber
+        $scriptname = $_.InvocationInfo.ScriptName
+        Write-Verbose "Write to log"
+        Write-ErrorLog -message $message -exception $exception -scriptname $scriptname -failinglinenumber $failinglinenumber -failingline $failingline -pscommandpath $pscommandpath -positionmsg $pscommandpath -stacktrace $strace
+        Write-Verbose "Errors written to log"
+    }
+}
+
+return Inspect-K8httpApplicationRouting
